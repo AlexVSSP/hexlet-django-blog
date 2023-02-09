8341b301bd5affdab75b0a31f9d25e1a1442aafe
Aleksei Seleznev <ferfa2@rambler.ru>
2023-02-09 19:56:00 +0300

    refactor view "index" in "article" app

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# def index(request):
+#     return HttpResponse('article')
+
+
 def index(request):
-    return HttpResponse('article')
+    tags = ['article']
+    return render(request, 'article/index.html', context={'tags': tags},)
