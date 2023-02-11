53f177004a7aeca454d44292e76b30056722d4eb
Aleksei Seleznev <ferfa2@rambler.ru>
2023-02-12 00:04:42 +0300

    add variables from .env

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8jfs2%!m=$(%v$!wohmr$)7vc1^&@fy6qjn*t(@e9ih$gru-+f'
+# SECRET_KEY = 'django-insecure-8jfs2%!m=$(%v$!wohmr$)7vc1^&@fy6qjn*t(@e9ih$gru-+f'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
