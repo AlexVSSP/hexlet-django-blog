9d6da19b8c6a6c2d4e0e20c9c9d879ca2c492986
Aleksei Seleznev <ferfa2@rambler.ru>
2023-02-14 20:24:42 +0300

    add "update" and "delete" functionality for articles

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 # from hexlet_django_blog.article import views
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView, ArticleFormEditView, ArticleFormDestroyView
 
 urlpatterns = [
     # path('', views.article),  # Закомитчено из-за 13. Список (CRUD)
     # path('', views.IndexView.as_view()),  # 7 Представления (Views)
     path('<str:tags>/<int:article_id>/', IndexView.as_view(), name='current_article'), # 8. Маршрутизация вместо предыдущ. строчки
     path('', IndexView.as_view()),  # 13. Список (CRUD)
+    path('<int:id>/update/', ArticleFormEditView.as_view(), name='articles_update'),  # 17. Обновление (CRUD)
+    path('<int:id>/delete/', ArticleFormDestroyView.as_view(), name='articles_destroy'),
     path('<int:id>/', ArticleView.as_view(), name='articles_detail'),  # 14. Просмотр (CRUD)
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),  # 16. Создание (CRUD)
 ]
